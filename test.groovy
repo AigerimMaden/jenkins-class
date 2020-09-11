32b9c83fe77826346d3a57fe18b20dc760eaf0cb
AigerimMaden <m_s_g_t@mail.ru>
2020-09-11 20:36:47 +0000

    added git hash

--- test.groovy
+++ test.groovy
@@ -1,5 +1,6 @@
 // Uniq name for the pod or slave 
 def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '')
 // yaml def for slaves 
 def slavePodTemplate = """
       metadata:
@@ -54,6 +55,7 @@ def slavePodTemplate = """
                     }
 
                     stage('Docker Push') {
+                        println(branch)
                         sh 'docker tag artemis aigerimmadenova/artemis'
                         sh 'docker push aigerimmadenova/artemis'
                      }
