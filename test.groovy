eb10c2ad8d382472fb605d3148dad82994e3e72e
AigerimMaden <m_s_g_t@mail.ru>
2020-09-11 21:22:18 +0000

    added git hash

--- test.groovy
+++ test.groovy
@@ -1,6 +1,13 @@
 // Uniq name for the pod or slave 
 def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
 def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '')
+
+properties([
+  [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+  parameters([
+    booleanParam(defaultValue: false, description: 'Select to be able to push to latest', name: 'pushLatest')
+    ])
+  ])
 // yaml def for slaves 
 def slavePodTemplate = """
       metadata:
